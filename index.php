ee9bb05a607652c05a770441baf9684e91be559b
Erik <vasquezerik611@gmail.com>
2023-11-18 17:41:21 -0600

    asdnasd1s
    nasnxjnx

--- index.php
+++ index.php
@@ -7,6 +7,12 @@
     <title>Plantilla básica de Bootstrap</title>
     <!-- CSS de Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
+    <style> 
+        .row {
+            margin: 20px;
+        }
+    </style>
 </head>
 
 <body>
